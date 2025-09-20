9751d46b3e7a5b87d1a5aee673d843b4d9a60e6a
EarthToMaher <maher.m.3rdlawgames@gmail.com>
2025-09-20 12:33:57 -0400

    Modified ReenableManager
    Now uses InvokeRepeating so we can run it every x seconds rather than every update
    Has a variable for the maximum amount of objects it can reenable each check
    When maxRespawnAmount < 1, it will respawn all objects
    
    This gives us more flexibility over reenable respawn so we can easily find that balance between gameplay and performance

--- Burning Rubble/Assets/Scripts/Destruction/ReenableManager.cs
+++ Burning Rubble/Assets/Scripts/Destruction/ReenableManager.cs
@@ -13,25 +13,38 @@ public class ReenableManager : MonoBehaviour
             eligibleTime = t;
         }
     }
-
+    [Tooltip("How often the reenable check runs in seconds")]
+    [SerializeField] private float checkRate = 3f;
+    [Tooltip("The maximum # of items it will respawn. Anything less than 1 will respawn all")]
+    [SerializeField] private int maxRespawnAmount = 5;
+    [Tooltip("The minimum amount of time an object must wait before being respawned")]
     [SerializeField] private float minWaitTime = 5f;
     private readonly List<Respawnable> pending = new();
 
     private float nextCheckTime;
 
-    void Update()
+    private void Start()
+    {
+        InvokeRepeating("ReenableClock", 0, checkRate);
+    }
+
+    public void ReenableClock()
     {
         if (pending.Count == 0) return;
         if (Time.time < nextCheckTime) return;
 
         // Re-enable all that are ready
         float now = Time.time;
+        int amountEnabled = 1; //Set to 1 so that way I can just do a greater than check instead of greater than or equal to
         for (int i = pending.Count - 1; i >= 0; i--)
         {
             if (pending[i].eligibleTime <= now)
             {
                 if (pending[i].obj) pending[i].obj.RepairMe();
                 pending.RemoveAt(i);
+                if (maxRespawnAmount < 1) continue;
+                amountEnabled++;
+                if (amountEnabled > maxRespawnAmount) break;
             }
         }
 
@@ -49,5 +62,12 @@ public class ReenableManager : MonoBehaviour
         pending.Add(new Respawnable(obj, Time.time + minWaitTime));
         if (pending.Count == 1 || Time.time + minWaitTime < nextCheckTime)
             nextCheckTime = Time.time + minWaitTime;
+    }
+
+    [ContextMenu("DestroyAll")]
+    public void DestroyAll()
+    {
+        DestructibleBlock[] blocks = FindObjectsByType<DestructibleBlock>(FindObjectsSortMode.None);
+        foreach (DestructibleBlock block in blocks) block.DestroyMe(this.gameObject,this.gameObject);
     }
 }
