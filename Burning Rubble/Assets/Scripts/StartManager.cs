a8cb1a2858f0d92ccbb5fd3ec0d88c4555784142
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2025-11-16 21:25:35 -0500

    possible rubble boost fix ft other issues that are not yet fixed

--- Burning Rubble/Assets/Scripts/StartManager.cs
+++ Burning Rubble/Assets/Scripts/StartManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputManager : MonoBehaviour
+public class StartManager : MonoBehaviour
 {
     public MPManager multiplayer;
     public PlayerInput playerInput;
